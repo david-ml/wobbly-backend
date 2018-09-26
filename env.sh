760b1df9f71b6ffdc52597e2fec3e2dfed77aba1
J <jdwatkins86@gmail.com>
2018-09-25 23:48:17 -0500

    permissions issue on docker-entrypoint.sh continues

--- env.sh
+++ env.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-docker rm -f `docker ps -aq -f name=wobbly-django*`
-set -a
-source .env
-cat ${COMPOSE_CONFIG} | envsubst | docker-compose up --build --abort-on-container-exit
-deactivate
+#set -a
+source /code/.env
